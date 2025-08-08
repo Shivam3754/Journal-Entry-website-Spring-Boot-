57f2eefb7c35da931e8126f6e3d22f26eba98cb3
root <root@lap0003546.yatraonline.local>
2025-08-08 10:04:30 +0530

    Service Annotation added to services

--- src/main/java/com/projectmania/journalApp/service/WeatherService.java
+++ src/main/java/com/projectmania/journalApp/service/WeatherService.java
@@ -2,14 +2,16 @@ package com.projectmania.journalApp.service;
 
 import com.projectmania.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
-    private static final String apiKey = "7ad0988f97d6d64d7f98e8bd12e6e5f0";
+    @Value("${weather.api.key}")
+    private String apiKey;
     private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
